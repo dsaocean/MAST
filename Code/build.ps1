8ce9d94d920602c3a8b53bdc97529282e35f091b
Paul <paul.mccarlie@dsaocean.com>
2023-06-06 10:36:11 -0700

    Added buidl script

---
+++ Code/build.ps1
@@ -0,0 +1,34 @@
+param (
+    [Parameter(Position = 0, Mandatory = $true)]
+    [string] $Version,
+
+    [Parameter(Position = 1, Mandatory = $true)]
+    [string] $Date,
+
+    [Parameter(Position = 2, Mandatory = $true)]
+    [string] $Platform,
+
+    [Parameter(Position = 3, Mandatory = $true)]
+    [scriptblock] $UpdateAssemblyVersion,
+
+    [Parameter(Position = 4, Mandatory = $true)]
+    [scriptblock] $UpdateBuildDate,
+
+    [Parameter(Position = 5, Mandatory = $true)]
+    [scriptblock] $RevertBuildDate,
+
+    [Parameter(Position = 6, Mandatory = $true)]
+    [string] $InstallationFolder,
+
+    [Parameter(Position = 7, Mandatory = $true)]
+    [string] $SolutionPath
+)
+
+$ProjectFolder = "$SolutionPath/ProteusDS MAST"
+$SolutionFile = "ProteusDS MAST.sln"
+
+UpdateAssemblyVersion "$ProjectFolder/Properties/AssemblyInfo.cs" $Version
+msbuild "$SolutionPath/$SolutionFile" -t:restore -p:RestorePackagesConfig=true
+msbuild "$SolutionPath/$SolutionFile" /p:Configuration=Release /p:Platform=$Platform
+New-Item -Path "$InstallationFolder\ProteusDS MAST" -ItemType Directory
+Copy-Item "$SolutionPath\bin\Release\$Platform\*" -Destination "$InstallationFolder\ProteusDS MAST" -Recurse
